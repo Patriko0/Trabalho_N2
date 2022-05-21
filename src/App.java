6dc48de7387358c2bce6df0a3b3a154e1a4e50b4
Patrik Américo <62946019+Patriko0@users.noreply.github.com>
2022-05-21 20:14:50 -0300

    Logica dos cursos

--- src/App.java
+++ src/App.java
@@ -23,6 +23,7 @@ public class App {
                     while (continuar) {
                         String New_nome;
                         int New_carg_hr;
+                        boolean exist;
                         System.out.println("\nGerenciar Disciplinas\n");
                         System.out.println(
                                 "Opções:\n   1-Cadastrar Disciplina\n   2-Consultar Disciplina\n   3-Remover disciplina\n   4-Atualizar disciplina\n   5-Voltar ao menu inicial");
@@ -46,7 +47,7 @@ public class App {
                                 for (Disciplinas i : disciplinas) {
                                     System.out.println(i.consulta());
                                 }
-                                boolean exist = false;
+                                exist = false;
                                 while (!exist) {
                                     System.out.println("\nEscreva o ID da disciplina que deseja consultar");
                                     opc = scan.nextInt();
@@ -131,7 +132,233 @@ public class App {
                     continuar = true;
                     break;
                 case 3:
-                    System.out.println("Gerenciar Cursos\n");
+                    while (continuar) {
+                        String New_nome;
+                        String New_turno;
+                        boolean exist = false;
+                        System.out.println("\nGerenciar Cursos\n");
+                        System.out.println(
+                                "Opções:\n   1-Cadastrar Curso\n   2-Consultar Curso\n   3-Remover Curso\n   4-Atualizar Curso\n   5-Adicionar nova disciplina a um Curso\n   6-Remover uma disciplina de um Curso\n   7-Voltar ao menu inicial");
+                        opc = scan.nextInt();
+                        switch (opc) {
+                            case 1:
+                                if (disciplinas.size() == 0) {
+                                    System.out.println("Nao existe nenhuma disciplina cadastrada");
+                                    break;
+                                }
+                                System.out.println("\nCadastrar Curso\n");
+                                scan.nextLine();
+                                System.out.println("Escreva o nome do Curso: ");
+                                New_nome = scan.nextLine();
+                                System.out.println("Escreva o turno do Curso: ");
+                                New_turno = scan.nextLine();
+
+                                for (Disciplinas i : disciplinas) {
+                                    System.out.println(i.consulta());
+                                }
+
+                                while (!exist) {
+                                    System.out.println("\nEscreva o Id da disciplina que deseja colocar inicialmente");
+                                    opc = scan.nextInt();
+                                    for (int i = 0; i < disciplinas.size(); i++) {
+                                        if (opc == disciplinas.get(i).getId()) {
+                                            exist = true;
+                                            cursos.add(new Curso(New_nome, New_turno, disciplinas.get(i)));
+                                        }
+                                    }
+                                    if (!exist) {
+                                        System.out.println("\nEscreva um ID de disciplina valido!");
+                                    }
+                                }
+                                break;
+                            case 2:
+                                if (cursos.size() == 0) {
+                                    System.out.println("\nNão existe nenhum Curso para ser Consultada!");
+                                    break;
+                                }
+                                System.out.println("\nConsultar Curso");
+                                for (Curso i : cursos) {
+                                    System.out.println(i.consulta());
+                                }
+                                exist = false;
+                                while (!exist) {
+                                    System.out.println("\nEscreva o ID do Curso que deseja consultar");
+                                    opc = scan.nextInt();
+                                    for (int i = 0; i < cursos.size(); i++) {
+                                        if (cursos.get(i).getId() == opc) {
+                                            exist = true;
+                                            System.out.println(cursos.get(i));
+                                        }
+                                    }
+                                    if (!exist) {
+                                        System.out.println("\nEscreva um ID valido!");
+                                    }
+                                }
+
+                                break;
+                            case 3:
+                                if (cursos.size() == 0) {
+                                    System.out.println("\nNão existe nenhum Curso para ser removida!");
+                                    break;
+                                }
+                                System.out.println("\nRemover Curso");
+                                for (Curso i : cursos) {
+                                    System.out.println(i.consulta());
+                                }
+                                exist = false;
+                                while (!exist) {
+                                    System.out.println("\nEscreva o ID do Curso que deseja Deletar");
+                                    opc = scan.nextInt();
+                                    for (int i = 0; i < cursos.size(); i++) {
+                                        if (cursos.get(i).getId() == opc) {
+                                            exist = true;
+                                            System.out.println("\nDeletando " + cursos.get(i).getNome());
+                                            cursos.remove(i);
+                                        }
+                                    }
+                                    if (!exist) {
+                                        System.out.println("\nEscreva um ID valido!");
+                                    }
+                                }
+
+                                break;
+                            case 4:
+                                if (cursos.size() == 0) {
+                                    System.out.println("\nNão existe nenhum Curso para ser Atualizada");
+                                    break;
+                                }
+                                System.out.println("\nAtualizar Curso\n");
+
+                                for (Curso i : cursos) {
+                                    System.out.println(i.consulta());
+                                }
+
+                                exist = false;
+                                while (!exist) {
+                                    System.out.println("\nEscreva o ID do curso que deseja Atualizar");
+                                    opc = scan.nextInt();
+                                    for (int i = 0; i < cursos.size(); i++) {
+                                        if (cursos.get(i).getId() == opc) {
+                                            exist = true;
+                                            scan.nextLine();
+                                            System.out.println("\nEscreve o novo nome para o Curso");
+                                            New_nome = scan.nextLine();
+                                            System.out.println("Escreva a novo turno do curso");
+                                            New_turno = scan.nextLine();
+                                            cursos.get(i).update(New_nome, New_turno);
+
+                                        }
+                                    }
+                                    if (!exist) {
+                                        System.out.println("\nEscreva um ID valido!");
+                                    }
+                                }
+                                break;
+                            case 5:
+                                if (cursos.size() == 0) {
+                                    System.out.println("\nNão existe nenhum Curso para colocar uma nova disciplina!");
+                                    break;
+                                }
+                                System.out.println("\nAdicionar nova disciplina a um Curso\n");
+
+                                for (Curso i : cursos) {
+                                    System.out.println(i.consulta());
+                                }
+                                exist = false;
+                                while (!exist) {
+                                    System.out.println(
+                                            "\nEscreva o ID do curso que deseja adicionar uma nova disciplina");
+                                    opc = scan.nextInt();
+                                    for (int i = 0; i < cursos.size(); i++) {
+                                        if (cursos.get(i).getId() == opc) {
+                                            exist = true;
+
+                                            for (Disciplinas e : disciplinas) {
+                                                System.out.println(e.consulta());
+                                            }
+
+                                            boolean exist_discip = false;
+
+                                            while (!exist_discip) {
+                                                System.out
+                                                        .println(
+                                                                "\nDigite o ID da disciplina que deseja ser adicionada");
+                                                opc = scan.nextInt();
+                                                for (int j = 0; j < disciplinas.size(); j++) {
+                                                    if (disciplinas.get(j).getId() == opc) {
+                                                        exist_discip = true;
+                                                        cursos.get(i).add_discip(disciplinas.get(j));
+                                                    }
+                                                }
+                                                if (!exist_discip) {
+                                                    System.out.println("Escreva um ID valido!");
+                                                }
+                                            }
+                                        }
+                                    }
+                                    if (!exist) {
+                                        System.out.println("\nEscreva um ID valido!");
+                                    }
+                                }
+                                break;
+                            case 6:
+                                if (cursos.size() == 0) {
+                                    System.out.println("\nNão existe nenhum Curso para remover uma disciplina!");
+                                    break;
+                                }
+                                System.out.println("\nRemover uma disciplina de um Curso\n");
+
+                                for (Curso i : cursos) {
+                                    System.out.println(i.consulta());
+                                }
+                                exist = false;
+                                while (!exist) {
+                                    System.out.println(
+                                            "\nEscreva o ID do curso que deseja remover uma disciplina");
+                                    opc = scan.nextInt();
+                                    for (int i = 0; i < cursos.size(); i++) {
+                                        if (cursos.get(i).getId() == opc) {
+                                            exist = true;
+
+                                            for (Disciplinas e : cursos.get(i).getDisp()) {
+                                                System.out.println(e.consulta());
+                                            }
+
+                                            boolean exist_discip = false;
+
+                                            while (!exist_discip) {
+                                                System.out
+                                                        .println(
+                                                                "\nDigite o ID da disciplina que voce deseja remover");
+                                                opc = scan.nextInt();
+                                                for (int j = 0; j < cursos.get(i).getDisp().size(); j++) {
+                                                    if (cursos.get(i).getDisp().get(j).getId() == opc) {
+                                                        exist_discip = true;
+                                                        cursos.get(i)
+                                                                .remove_discip(cursos.get(i).getDisp().get(j).getId());
+                                                    }
+                                                }
+                                                if (!exist_discip) {
+                                                    System.out.println("Escreva um ID valido!");
+                                                }
+                                            }
+                                        }
+                                    }
+                                    if (!exist) {
+                                        System.out.println("\nEscreva um ID valido!");
+                                    }
+                                }
+                                break;
+                            case 7:
+                                System.out.println("\nVoltando ao menu principal...\n");
+                                continuar = false;
+                                break;
+                            default:
+                                System.out.println("\nEscolha uma opcao valida\n");
+                                break;
+                        }
+                    }
+                    continuar = true;
                     break;
                 case 4:
                     System.out.println("Saindo...\n");
--- src/Curso.java
+++ src/Curso.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Curso {
-    static private int qts_cursos = 0;
+    static private int new_cursos_id = 0;
     private int id;
     private String nome;
     private String turno;
@@ -11,12 +11,20 @@ public class Curso {
         this.nome = nome;
         this.turno = turno;
         this.disp.add(disp);
-        qts_cursos++;
-        id = qts_cursos;
+        new_cursos_id++;
+        id = new_cursos_id;
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public ArrayList<Disciplinas> getDisp() {
+        return disp;
     }
 
     public void setNewDisp(Disciplinas disp) {
@@ -28,6 +36,28 @@ public class Curso {
         for (Disciplinas i : disp) {
             nome_disp += i.getNome() + " | ";
         }
-        return "Id: "+ id + "\nCurso: " + nome + "\nTurno: " + turno + "\nDisciplinas: " + nome_disp+ "\n";
+
+        return String.format("\nId: %d\nCurso: %s\nTurno: %s\nDisciplinas: %s\n", id, nome, turno, nome_disp);
+    }
+
+    public String consulta() {
+        return String.format("\nId: %d\nCurso: %s", id, nome);
+    }
+
+    public void add_discip(Disciplinas d) {
+        disp.add(d);
+    }
+
+    public void remove_discip(int id_discip) {
+        for (int i = 0; i < disp.size(); i++) {
+            if (disp.get(i).getId() == id_discip) {
+                disp.remove(i);
+            }
+        }
+    }
+
+    public void update(String nome, String turno) {
+        this.nome = nome;
+        this.turno = turno;
     }
 }
